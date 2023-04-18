e5409e973b16e2184c9795dc02d8a15de5e826b6
Viccart <hannahcarter89@hotmail.com>
2023-04-18 17:46:54 +0100

    Refactor header link

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import ArticlesList from "./components/ArticlesList";
 import SingleArticle from "./components/SingleArticle";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 
 function App() {
-  const [showArticlesList, setShowArticlesList] = useState(false);
-  const navigate = useNavigate();
-
-  const handleHeaderClick = () => {
-    setShowArticlesList(!showArticlesList);
-    navigate("/articles");
-  };
 
   return (
     <div className="App">
-      <Header onClick={handleHeaderClick} />
+      <Header />
       <Nav />
       <main className="articles-list">
         <Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header({ onClick }) {
   return (
-    <header id="header" onClick={onClick}>
-      <h1>News Now</h1>
+    <header id="header">
+      <Link to={`/articles`}>
+        <h1>News Now</h1>
+      </Link>
     </header>
   );
 }
